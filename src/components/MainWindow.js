99a93a4759b73aa851668280026cf9b41c09523d
ilyakoptev <ilya.koptev@gmail.com>
2019-10-11 22:06:10 +0300

    add swith by menu click to mainWindow

--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -40,13 +40,137 @@ class MainWindow extends React.Component {
         //console.log(employees)
         const currectUser = employees[0].data.find( (item)=> {if (item.EmployeeId == activeUser) return item.Name} ) //get all data of currect user 
         //console.log(customerorders)
-        if(menuChoose==="createNewCustOrder"){
-          return(
-            <div>Create New Order</div>
-          ) 
-        }
+       //conditional rendering by menu item clicked
+       switch(menuChoose) {
+        case "createNewCustOrder": 
+                    {
+                         return(
+                                      <div>Create New Order</div>
+                                 )  
+                     };
+        case "addNewCustomer": 
+                    {
+                         return(
+                                      <div>addNewCustomer</div>
+                                 )  
+                     };
+        case "editCustomer": 
+          {
+               return(
+                            <div>editCustomer</div>
+                       )  
+           };
+           case "showCustomers": 
+           {
+                return(
+                             <div>showCustomers</div>
+                        )  
+            };
+            case "createNewSuppOrder": 
+            {
+                 return(
+                              <div>createNewSuppOrder</div>
+                         )  
+             };
+             case "editSupplier": 
+          {
+               return(
+                            <div>editSupplier</div>
+                       )  
+           };
+           case "showSuppliers": 
+          {
+               return(
+                            <div>showSuppliers</div>
+                       )  
+           };
+           case "addNewProduct": 
+          {
+               return(
+                            <div>addNewProduct</div>
+                       )  
+           };
+           case "editProduct": 
+          {
+               return(
+                            <div>editProduct</div>
+                       )  
+           };
+           case "deleteProduct": 
+          {
+               return(
+                            <div>deleteProduct</div>
+                       )  
+           };
+           case "showProducts": 
+          {
+               return(
+                            <div>showProducts</div>
+                       )  
+           }; 
+           case "newReciept": 
+          {
+               return(
+                            <div>newReciept</div>
+                       )  
+           };  
+           case "newPayment": 
+          {
+               return(
+                            <div>newPayment</div>
+                       )  
+           };  
+           case "custBalance": 
+          {
+               return(
+                            <div>custBalance</div>
+                       )  
+           };  
+           case "suppBalance": 
+          {
+               return(
+                            <div>suppBalance</div>
+                       )  
+           };  
+           case "showSaldo": 
+          {
+               return(
+                            <div>showSaldo</div>
+                       )  
+           };     
+           case "addNewEmployee": 
+          {
+               return(
+                            <div>addNewEmployee</div>
+                       )  
+           };  
+           case "editEmployee": 
+          {
+               return(
+                            <div>editEmployee</div>
+                       )  
+           };  
+           case "showProducts": 
+          {
+               return(
+                            <div>showProducts</div>
+                       )  
+           };  
+           case "deleteEmployeer": 
+          {
+               return(
+                            <div>deleteEmployeer</div>
+                       )  
+           };  
+           case "showEmployees": 
+          {
+               return(
+                            <div>showEmployees</div>
+                       )  
+           };  
+          
 
-        else{
+        default : 
             return (
                 <div>
                   <h1>Users</h1>
@@ -59,7 +183,7 @@ class MainWindow extends React.Component {
                    
                 </div>
 
-             ) }
+             ) };
     }
 }
 
